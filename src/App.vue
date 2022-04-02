3555fc4240b35ae4a2d07a204c04e5dfb44aadfe
Minh triet <trietnguyen020@gmail.com>
2022-04-02 23:42:03 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 export default {
 
 }
@@ -6,6 +6,33 @@ export default {
 
 <template>
     <h1>Hello, Vue.js!</h1>
+</template>
+
+<style>
+.page {
+    max-width: 400px;
+    margin: auto;
+}
+</style> -->
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+    components: {
+        AppHeader,
+    },
+};
+</script>
+
+<template>
+    <div id="app">
+        <AppHeader />
+
+        <div class="container mt-3">
+            <router-view />
+        </div>
+    </div>
 </template>
 
 <style>
---
+++ src/components/ContactCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props: {
+        contact: { type: Object, required: true },
+    },
+};
+</script>
+
+<template>
+    <div class="row">
+        <div class="pt-2 col-12">
+            <strong>Tên:</strong>
+            {{ contact.name }}
+        </div>
+        <div class="pt-2 col-12">
+            <strong>E-mail:</strong>
+            {{ contact.email }}
+        </div>
+        <div class="pt-2 col-12">
+            <strong>Địa chỉ:</strong>
+            {{ contact.address }}
+        </div>
+        <div class="pt-2 col-12">
+            <strong>Điện thoại:</strong>
+            {{ contact.phone }}
+        </div>
+        <div class="pt-2 col-12">
+            <strong>Liên hệ yêu thích:&nbsp;</strong>
+            <i v-if="contact.favorite" class="fas fa-check"></i>
+            <i v-else class="fas fa-times"></i>
+        </div>
+    </div>
+</template>
